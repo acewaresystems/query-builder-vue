4a3072430035fa332be9ae1578f0615198049d37
Brandon Klimek <brandon.klimek@jobget.com>
2023-08-05 21:50:05 -0500

    made enum into constants.ts

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+export const enum RuleSetComparator {
+  AND = 'AND',
+  OR = 'OR'
+}
+
+export const enum RuleConditions {
+  EQUALS='EQUALS',
+  NOT_EQUALS='NOT_EQUALS',
+  GREATER_THAN='GREATER_THAN',
+  LESS_THAN='LESS_THAN',
+  LIKE='LIKE',
+  STARTS_WITH='STARTS_WITH',
+  ENDS_WITH='ENDS_WITH',
+  CONTAINS='CONTAINS',
+  IS_NULL='IS_NULL',
+  NOT_NULL='NOT_NULL'
+}
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,22 +1,7 @@
 import { Component } from 'vue';
 import { SortableOptions } from 'sortablejs';
-export const enum RuleSetComparator {
-  AND = 'AND',
-  OR = 'OR'
-}
+import {RuleConditions, RuleSetComparator} from "../src/constants";
 
-export const enum RuleConditions {
-  EQUALS='EQUALS',
-  NOT_EQUALS='NOT_EQUALS',
-  GREATER_THAN='GREATER_THAN',
-  LESS_THAN='LESS_THAN',
-  LIKE='LIKE',
-  STARTS_WITH='STARTS_WITH',
-  ENDS_WITH='ENDS_WITH',
-  CONTAINS='CONTAINS',
-  IS_NULL='IS_NULL',
-  NOT_NULL='NOT_NULL'
-}
 
 export interface Rule {
   column: string
